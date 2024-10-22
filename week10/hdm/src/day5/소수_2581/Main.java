52bf7f0c6760000ca5eebc9466c418c0b18003fe
min <m2insapp@gmail.com>
2024-10-22 15:01:15 +0900

    feat: week10 day4

---
+++ week10/hdm/src/day5/소수_2581/Main.java
@@ -0,0 +1,20 @@
+package day5.소수_2581;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        String file = "src/day5/소수_2581/input.txt";
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        /*
+        
+         */
+
+
+
+    }
+}
